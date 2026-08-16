385339a85fa066fb6102be31bc6fc7b886800e2e
ChandrashekharSahu-981 <sahuchandrashekhar981@gmail.com>
2026-08-16 10:43:43 +0530

    Update IndiaStates game

--- 18_IndiaStates_Game/main.py
+++ 18_IndiaStates_Game/main.py
@@ -23,10 +23,7 @@ while len(guessed_states) < 28:
     answer_state = answer_state.title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         file_path = Path(__file__).parent / "missing_states.csv"
         new_data.to_csv(file_path,index=False)
